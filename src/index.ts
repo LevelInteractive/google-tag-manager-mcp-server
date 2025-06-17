ef041026e74a1ff6ecd6c720ed93f28bef2e4fe6
Dave Brong <7083272+davebrong@users.noreply.github.com>
2025-06-17 13:33:59 -0400

    Swap to Streamable HTTP

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,20 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { getPackageVersion, loadEnv, log } from "./utils";
-import { tools } from "./tools";
+import { ExpressHttpStreamableMcpServer } from "./server_runner.js";
+import { getPackageVersion, loadEnv } from "./utils/index.js";
+import { tools } from "./tools/index.js";
 
 loadEnv();
 
-const server = new McpServer({
-  name: "google-tag-manager",
-  version: getPackageVersion(),
-  protocolVersion: "1.0",
-  vendor: "stape-io",
-  homepage: "https://github.com/stape-io/google-tag-manager-mcp-server",
-});
+console.log("Initializing MCP Streamable-HTTP Server with Express");
 
-tools.forEach((register) => register(server));
-
-async function main(): Promise<void> {
-  try {
-    log("Starting MCP server with stdio transport...");
-    const transport = new StdioServerTransport();
-    await server.connect(transport);
-    log("✅ MCP server started");
-  } catch (error) {
-    log(`❌ Error starting server: ${error}`);
-    process.exit(1);
-  }
-}
-
-main();
+ExpressHttpStreamableMcpServer(
+  {
+    name: "google-tag-manager",
+    version: getPackageVersion(),
+    protocolVersion: "1.0",
+    vendor: "stape-io",
+    homepage: "https://github.com/stape-io/google-tag-manager-mcp-server",
+  },
+  (server) => {
+    tools.forEach((register) => register(server));
+  },
+);
